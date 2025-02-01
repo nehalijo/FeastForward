8580fdb9a392cb3469dff12b210e1246cee3df18
Neha <neha.lijo.22@gmail.com>
2025-02-01 21:36:38 +0530

    Added donor details accepting

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
+const cors = require("cors");
 const authRoutes = require("./auth");
+const db = require("./db");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(
@@ -17,6 +20,22 @@ app.use(
 );
 
 app.use("/auth", authRoutes);
+
+app.post("/api/donors", async (req, res) => {
+  const {email, foodname, quantity, pickup_long, pickup_lat, expiration, food_type} = req.body;
+
+  try {
+    const sql = 'INSERT INTO food (email, foodname, quantity, pickup_long, pickup_lat, expiration, food_type) VALUES (?,?,?,?,?,?,?)';
+    const values = [email, foodname, quantity, pickup_long, pickup_lat, expiration, food_type];
+
+    const [result] = await db.query(sql, values);
+
+    res.json({ message: "Donation successful"});
+  } catch(err) {
+      console.error("Error inserting data",err);
+      res.status(500).send("Database error");
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
