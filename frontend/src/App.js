077bb167cede5456594c52f87acba7737795e51e
Steffi <steffilazar01@gmail.com>
2025-02-02 02:36:31 +0530

    edited file structure

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+//simport logo from './logo.svg';
+import LoginSignup from './components/loginsignup/login'
+import Home from './components/home/home'
+import './App.css';
+
+function App() {
+  console.log(12)
+  return (
+    <div>
+      <LoginSignup> </LoginSignup>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
